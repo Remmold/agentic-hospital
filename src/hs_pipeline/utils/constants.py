8b16e1b6a076c464f63dc7b6323d015e7403eb16
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-10-18 13:16:47 +0200

    Added llm type to constants

--- src/hs_pipeline/utils/constants.py
+++ src/hs_pipeline/utils/constants.py
@@ -1,4 +1,9 @@
 import os
 from pathlib import Path
+"""Paths"""
 PROJECT_ROOT = Path(__file__).parents[1]
 DATA_PATH = PROJECT_ROOT / "ocr" / "data"
+AGENT_JSON_STRUCTURE_PATH = PROJECT_ROOT / "agents" / "json_structures"
+
+"""LLM constants"""
+CHOSEN_LLM = 'gemini-2.5-flash'
