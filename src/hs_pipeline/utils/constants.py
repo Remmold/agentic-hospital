1beed241b0e424291630df69a7f1faaea25e9c24
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-10-20 14:41:43 +0200

    add openai (through azure) support

--- src/hs_pipeline/utils/constants.py
+++ src/hs_pipeline/utils/constants.py
@@ -1,4 +1,10 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+from pydantic_ai.models.openai import OpenAIChatModel
+
+load_dotenv()
+
 """Paths"""
 PROJECT_ROOT = Path(__file__).parents[1]
 DATA_PATH = PROJECT_ROOT / "ocr" / "data"
@@ -9,5 +15,10 @@ GEMINI_LLM = 'gemini-2.5-flash' #Currently functions well for agent_loop but 10 
 LAMA_VERSATILE_LLM = 'groq:llama-3.3-70b-versatile' #currently non functional for agent_loop
 LAMA_INSTANT_LLM = 'groq:llama-3.1-8b-instant' #Terrible for agent_loop
 
+CHATGPT_LLM = OpenAIChatModel(
+    model_name=os.getenv('AZURE_OPENAI_DEPLOYMENT'),  # 'gpt-4o-mini'
+    provider='azure'
+)
+
 # LLM used throughout the project
-CHOSEN_LLM = GEMINI_LLM
+CHOSEN_LLM = CHATGPT_LLM
