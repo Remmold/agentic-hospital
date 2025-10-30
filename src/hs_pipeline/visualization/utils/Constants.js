0d07a908e908cc1bba142df4195a2c47b1d88f7e
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-10-30 15:27:14 +0100

    Add constants utilityfile with (x,y) coordinates for important locations

--- src/hs_pipeline/visualization/utils/Constants.js
+++ src/hs_pipeline/visualization/utils/Constants.js
@@ -4,27 +4,33 @@ export const LOCATIONS = {
     ENTRANCE: { x: 21 * 32, y: 33 * 32, name: 'Hospital Entrance' },
     
     // Examination Rooms
-    NURSE_OFFICE: { x: 32 * 32, y: 18 * 32, name: 'Examination Room 1' }, // Nurses office
+    NURSE_OFFICE: { x: 34 * 32, y: 18 * 32, name: 'Examination Room 1' }, // Nurses office
     DOCTORS_OFFICE: { x: 37 * 32, y: 12 * 32, name: 'Examination Room 2' }, // Doctors office
-    EXAM_ROOM_3: { x: 30 * 32, y: 10 * 32, name: 'Examination Room 3' },
-    
-    WAITING_AREA_1: { x: 15 * 32, y: 25 * 32, name: 'Waiting Area 1' },
-    WAITING_AREA_2: { x: 25 * 32, y: 25 * 32, name: 'Waiting Area 2' },
     
     RECEPTION: { x: 17 * 32, y: 26 * 32, name: 'Reception' },
-    LAB: { x: 20 * 32, y: 20 * 32, name: 'Laboratory' },
-    
-    HALLWAY_NORTH: { x: 20 * 32, y: 15 * 32, name: 'North Hallway' },
-    HALLWAY_SOUTH: { x: 20 * 32, y: 35 * 32, name: 'South Hallway' },
-    HALLWAY_WEST: { x: 10 * 32, y: 25 * 32, name: 'West Hallway' },
-    HALLWAY_EAST: { x: 30 * 32, y: 25 * 32, name: 'East Hallway' }
+    LAB_DEFAULT: { x: 20 * 32, y: 20 * 32, name: 'Laboratory' },
+    LAB_MRI: { x: 2 * 32, y: 7 * 32, name: 'MRI Room' },
+    LAB_XRAY: { x: 37 * 32, y: 2 * 32, name: 'X-Ray Room' },
+    LAB_BLOOD: { x: 32 * 32, y: 15 * 32, name: 'Blood Test Room' },
+    LAB_CT: { x: 38 * 32, y: 18 * 32, name: 'CT Scan Room' },
+
 };
 
 // Map agent types to locations
 export const AGENT_LOCATIONS = {
     'Nurse': 'NURSE_OFFICE',
     'Doctor': 'DOCTORS_OFFICE',
-    'Lab': 'LAB'
+    'Lab': 'LAB_DEFAULT'
+};
+
+// Map test types to lab locations
+export const LAB_TEST_LOCATIONS = {
+    'mri': 'LAB_MRI',
+    'xray': 'LAB_XRAY',
+    'x-ray': 'LAB_XRAY',
+    'blood': 'LAB_DEFAULT',
+    'ct': 'LAB_MRI',
+    'ct scan': 'LAB_MRI'
 };
 
 // Helper to get random location
